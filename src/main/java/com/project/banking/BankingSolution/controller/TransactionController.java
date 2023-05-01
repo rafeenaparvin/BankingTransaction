18a9a63769ff3569cebcbb7d721673fbef1008c1
Rafeena Parvin <rafeena.parvin@aspiresys.com>
2023-05-01 20:17:03 +0530

    Added purpose on controller methods

--- src/main/java/com/project/banking/BankingSolution/controller/TransactionController.java
+++ src/main/java/com/project/banking/BankingSolution/controller/TransactionController.java
@@ -2,6 +2,7 @@ package com.project.banking.BankingSolution.controller;
 
 import com.project.banking.BankingSolution.model.Transaction;
 import com.project.banking.BankingSolution.service.TransactionService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,14 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @PostMapping(value = "/credit")
+    @ApiOperation("credit the money to the given account ID")
     public ResponseEntity creditMoney(@RequestBody Transaction transaction) {
         transactionService.creditMoney(transaction);
         return new ResponseEntity("Transfer is successful", HttpStatus.OK);
     }
 
     @PostMapping(value = "/debit")
+    @ApiOperation("Debit the money to the given account ID")
     public ResponseEntity debitMoney(@RequestBody Transaction transaction) {
         transactionService.debitMoney(transaction);
         return new ResponseEntity("Transfer is successful", HttpStatus.OK);
